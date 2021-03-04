f0f241697d32870efbc91adaee5a51a07d93ee3a
Damian <damianionut990@gmail.com>
2021-03-04 08:23:21 +0200

    Created readers; deleted utils

--- Seminar2_CTS/src/ro/ase/cts/main/Program.java
+++ Seminar2_CTS/src/ro/ase/cts/main/Program.java
@@ -1,4 +1,9 @@
-package ro.ase.cts.classes;
+package ro.ase.cts.main;
+
+
+import ro.ase.cts.classes.*;
+import ro.ase.cts.readers.*;
+import ro.ase.cts.utils.*;
 
 import java.io.FileNotFoundException;
 import java.util.List;
@@ -8,7 +13,7 @@ public class Program {
 	public static void main(String[] args) {
 		List<Angajat> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
 			for(Angajat angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
