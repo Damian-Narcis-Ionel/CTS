7e5d572e329830b77299c8cb0dfaffe98bb06c08
Damian <damianionut990@gmail.com>
2021-03-18 08:26:43 +0200

    Eager + lazy

--- Seminar4_CTS/src/ro/ase/cts/clase/ClinicaVeterinaraEager.java
+++ Seminar4_CTS/src/ro/ase/cts/clase/ClinicaVeterinaraEager.java
@@ -1,5 +1,34 @@
 package ro.ase.cts.clase;
 
 public class ClinicaVeterinaraEager {
+	private String nume;
+	private String adresa;
+	private int nrMedici;
+	private float buget;
+	//Instanta initializata la momentul declararii la EagerInitialization
+	private static final ClinicaVeterinaraEager clinica =
+			new ClinicaVeterinaraEager("Clinica", "Str. Dorobanti", 30, 300.5f);
+	
+	// Constructorul trebuie sa fie private obligatoriu
+	private ClinicaVeterinaraEager(String nume, String adresa, int nrMedici, float buget) {
+		super();
+		this.nume = nume;
+		this.adresa = adresa;
+		this.nrMedici = nrMedici;
+		this.buget = buget;
+	}
+	
+	//Returneaza instanta privata a clasei
+	public static synchronized ClinicaVeterinaraEager getInstance() {
+		return clinica;
+	}
 
+	@Override
+	public String toString() {
+		return "ClinicaVeterinara [nume=" + nume + ", adresa=" + adresa + ", nrMedici=" + nrMedici + ", buget=" + buget
+				+ "]";
+	}
+
+	
+	
 }
--- Seminar4_CTS/src/ro/ase/cts/main/Program.java
+++ Seminar4_CTS/src/ro/ase/cts/main/Program.java
@@ -1,10 +1,23 @@
 package ro.ase.cts.main;
+import ro.ase.cts.clase.*;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		ClinicaVeterinara clinica1 = ClinicaVeterinara.getInstance();
+//		ClinicaVeterinara clinica2 = ClinicaVeterinara.getInstance();
+//		
+//		System.out.println(clinica1.toString());
+//		System.out.println(clinica2.toString());
 		
+		ClinicaVeterinaraLazy clinicaLazy1 = ClinicaVeterinaraLazy.getInstance("Clinica Zurli", "Str. Berceni", 30, 560.04f);
+		ClinicaVeterinaraLazy clinicaLazy2 = ClinicaVeterinaraLazy.getInstance("NU se mai schimba", "Str. neschimbata", 60, 421.43f);
+
+		clinicaLazy1.setNume("Clinica modificata");
+		clinicaLazy2.setNrMedici(70);
+		
+		System.out.println(clinicaLazy1.toString());
+		System.out.println(clinicaLazy2.toString());
 	}
 
 }
